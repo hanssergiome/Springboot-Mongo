9d37c2ddb82880dacea6ca10296472335216db7e
Hans Sérgio <hanssergiom@gmail.com>
2022-03-13 14:59:32 -0300

    Data Transfer Object

--- workshopmongo/src/main/java/com/hanssergiom/workshopmongo/resources/UserResources.java
+++ workshopmongo/src/main/java/com/hanssergiom/workshopmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.hanssergiom.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,20 +10,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hanssergiom.workshopmongo.domain.User;
+import com.hanssergiom.workshopmongo.dto.UserDTO;
 import com.hanssergiom.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 
-	@Autowired 
+	@Autowired
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
-		
+	public ResponseEntity<List<UserDTO>>findAll() {
+		//DATA TRANSFER OBJECT Camada de visão (view layer) e outra de persistência dos dados (model layer).
 		List<User> list =service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto=list.stream().map(x-> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 
 	}
 
