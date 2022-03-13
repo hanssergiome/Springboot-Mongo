8bd0df66501928130a5687968357444e71d64551
Hans Sérgio <hanssergiom@gmail.com>
2022-03-13 18:32:38 -0300

    Update User Postman

--- workshopmongo/src/main/java/com/hanssergiom/workshopmongo/resources/UserResources.java
+++ workshopmongo/src/main/java/com/hanssergiom/workshopmongo/resources/UserResources.java
@@ -55,4 +55,12 @@ public class UserResources {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build(); 
+	}
 }
--- workshopmongo/src/main/java/com/hanssergiom/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/hanssergiom/workshopmongo/services/UserService.java
@@ -41,6 +41,17 @@ public class UserService {
 		repo.deleteById(id);
 		
 	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		}
+
+	public void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 
 	public User fromDTO(UserDTO objDto) {
 
