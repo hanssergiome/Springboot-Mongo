ebe25dbdcabaae251395de05d076376e12215548
Hans Sérgio <hanssergiom@gmail.com>
2022-03-13 09:08:39 -0300

    Users Class Tested in Postman

---
+++ workshopmongo/src/main/java/com/hanssergiom/workshopmongo/domain/User.java
@@ -0,0 +1,70 @@
+package com.hanssergiom.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+// Serializable - objetos convertidos em bytes para serem trafegados em redes ou gravados em arquivo.
+public class User implements Serializable {
+    //User setado como Serial Padrão
+	private static final long serialVersionUID = 1L;
+	// Classe Segundo Projeto
+	private String id;
+	private String name;
+	private String email;
+
+	public User() {
+
+	}
+
+// Source Construtores
+	public User(String id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	// Source Gets and Setters
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	// Source Hash/Equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
